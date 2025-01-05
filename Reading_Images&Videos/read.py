e5643c0b524fd1fc132952f62685c4c34624640d
AdityaGaur7 <adgaur027@gmail.com>
2025-01-05 12:33:32 +0530

    reading images and videos

---
+++ Reading_Images&Videos/read.py
@@ -0,0 +1,22 @@
+import cv2 as cv 
+
+# Reading Images
+img = cv.imread('Photos/1.png')
+
+cv.imshow('1', img)
+
+cv.waitKey(0)
+
+
+# Reading Videos
+# capture = cv.VideoCapture('Videos/download.mp4')
+
+# while True:
+#     isTrue, frame = capture.read()
+#     cv.imshow('Video', frame)
+
+#     if cv.waitKey(20) & 0xFF==ord('d'):
+#         break
+
+# capture.release()
+# cv.destroyAllWindows()
