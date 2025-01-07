4aa12e4a581e93789587e4b192011ecc3b2fde9d
AdityaGaur7 <adgaur027@gmail.com>
2025-01-07 23:13:23 +0530

    blurring

---
+++ smoothing.py
@@ -0,0 +1,27 @@
+import cv2 as cv 
+
+img = cv.imread('Photos/1.png')
+cv.imshow('1', img)
+
+#Averaging
+average = cv.blur(img, (7,7))
+cv.imshow('Average Blur', average)
+
+#Gaussian Blur
+gauss = cv.GaussianBlur(img, (7,7), 0)
+cv.imshow('Gaussian Blur', gauss)
+
+#Median Blur
+median = cv.medianBlur(img, 7)
+cv.imshow('Median Blur', median)
+
+#bilateral
+bilateral = cv.bilateralFilter(img, 10, 35, 25)
+cv.imshow('Bilateral', bilateral)
+
+
+
+
+
+
+cv.waitKey(0)
