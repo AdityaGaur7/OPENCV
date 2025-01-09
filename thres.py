3a66c574ab2c3557721e47e591186bfd79269706
AdityaGaur7 <adgaur027@gmail.com>
2025-01-09 14:07:17 +0530

    thresholding

---
+++ thres.py
@@ -0,0 +1,31 @@
+import cv2 as cv
+
+import numpy as np
+
+img = cv.imread('Photos/1.png')
+cv.imshow('1', img)
+
+
+#simple thresholding
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray', gray)
+
+#Simple Thresholding
+threshold, thresh = cv.threshold(gray, 150, 255, cv.THRESH_BINARY)
+cv.imshow('Simple Thresholded', thresh)
+
+#Simple Thresholding Inverse
+threshold, thresh_inv = cv.threshold(gray, 150, 255, cv.THRESH_BINARY_INV)
+cv.imshow('Simple Thresholded Inverse', thresh_inv)
+
+#Adaptive Thresholding
+adaptive_thresh = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 3)
+cv.imshow('Adaptive Thresholding', adaptive_thresh)
+
+adaptive_thresh_gaussian = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 3)
+cv.imshow('Adaptive Thresholding Gaussian', adaptive_thresh_gaussian)
+
+
+
+
+cv.waitKey(0)
